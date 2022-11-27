99fd4f11599927920ef847d6a520fcad3a77dd70
Andrey Nefedov <andryhaee@yandex.ru>
2022-11-27 21:51:32 +0500

    Вторая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,25 @@ const popup = document.querySelector('.popup');
 const popupBtnClose = popup.querySelector('.popup__btn-close');
 const popupBtnOpen = document.querySelector('.profile__edit-button');
 const formElement = document.querySelector('.popup__content');
-const nameInput = popup.querySelector('.popup__name');
-const professionInput = popup.querySelector('.popup__profession');
+const nameInput = popup.querySelector('.popup__input_value_name');
+const professionInput = popup.querySelector('.popup__input_value_profession');
 const profileName = document.querySelector('.profile__title');
 const profileProfession = document.querySelector('.profile__subtitle');
 
 
 // Открытие попапа
 const popupOpen = function  () {
-  popup.classList.add('popup_is-opened');
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   professionInput.value = profileProfession.textContent;
 }
 
 // Закрытие попапа
 const popupClose = function  () {
-  popup.classList.remove('popup_is-opened');
+  popup.classList.remove('popup_opened');
 }
 
-// Закрытие попапа при клике вне попапа
-const closePopupByClickOnOverlay = function (event) {
-  if (event.target !== event.currentTarget) {
-    retern;
-  }
-  popupClose();
-}
-
+//Обработка отправки введенных в попап данных
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -39,7 +32,6 @@ function formSubmitHandler (evt) {
 // Обработчики событий
 popupBtnOpen.addEventListener('click', popupOpen);
 popupBtnClose.addEventListener('click', popupClose);
-popup.addEventListener('click', closePopupByClickOnOverlay);
 formElement.addEventListener('submit', formSubmitHandler); 
 
 
